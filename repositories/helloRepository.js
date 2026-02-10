5f6f749ebaea7d77cf737eb753710dae0876adba
Mores20 <morrelukerz@gmail.com>
2026-02-11 07:30:05 +0800

    Delete repositories directory
    cleaning

--- repositories/helloRepository.js
+++
@@ -1,7 +0,0 @@
-const HelloModel = require('../models/helloModel');
-
-exports.fetchGreeting = () => {
-  // simulate data access layer
-  const model = new HelloModel('Hello from backend (model)');
-  return model;
-};
